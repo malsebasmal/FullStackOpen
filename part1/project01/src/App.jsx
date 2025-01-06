98a98e92845620f7228f42cbf5074052b17201a5
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-06 16:19:14 -0500

    new component in the same page

--- part1/project01/src/App.jsx
+++ part1/project01/src/App.jsx
@@ -1,39 +1,21 @@
 import './App.css'
 
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello word / Hola mundo!</h1>
+    </div>
+  )
+}
+
 const App = () => {
   const now = new Date()
-  const a = 20
-  const b = 5
-  console.log(now, a + b)
 
   return (
     <div>
-      <p>Hello word / Hola mundo!</p>
-      <p>
-        Today the date is {now.toDateString()}
-      </p>
-      <ul>
-        <li>
-          <p>
-            {a} plus {b} is {a + b}
-          </p>
-        </li>
-        <li>
-          <p>
-            {a} unless {b} is {a - b}
-          </p>
-        </li>
-        <li>
-          <p>
-            {a} by {b} is {a * b}
-          </p>
-        </li>
-        <li>
-          <p>
-            {a} divided {b} is {a * b}
-          </p>
-        </li>
-      </ul>
+      <h1>Saludos:</h1>
+      <Hello />
+      <p>Date: {now.toDateString()}</p>
     </div>
   )
 }
