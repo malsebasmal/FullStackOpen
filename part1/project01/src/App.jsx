7cfb7f5bcb4c3219a5e839518a8b70150879a8d3
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-06 15:51:07 -0500

    first steps in react

---
+++ part1/project01/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+const App = () => {
+  const now = new Date()
+  const a = 20
+  const b = 5
+  console.log(now, a + b)
+
+  return (
+    <div>
+      <p>Hello word / Hola mundo!</p>
+      <ul>
+        <li>
+          <p>
+            {a} plus {b} is {a + b}
+          </p>
+        </li>
+        <li>
+          <p>
+            {a} unless {b} is {a - b}
+          </p>
+        </li>
+        <li>
+          <p>
+            {a} by {b} is {a * b}
+          </p>
+        </li>
+        <li>
+          <p>
+            {a} divided {b} is {a * b}
+          </p>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
