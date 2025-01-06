26242e56b56b2a8c7fdbe3c8ab942ecdf6d38b52
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-06 16:48:20 -0500

    add eslint config for not use prop-types

--- part1/project01/src/App.jsx
+++ part1/project01/src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css'
 
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
+
   return (
     <div>
-      <h1>Hello word / Hola mundo!</h1>
+      <h1>Hello word and {props.name}, you age {props.age} years old</h1>
     </div>
   )
 }
 
 const App = () => {
   const now = new Date()
+  const name = "Peter"
+  const age = 25
 
   return (
     <div>
-      <h1>Saludos:</h1>
-      <Hello />
+      <h1>Greetings:</h1>
+      <Hello name="Thomas" age={10 + 5}/>
+      <Hello name={name} age={age}/>
       <p>Date: {now.toDateString()}</p>
     </div>
   )
