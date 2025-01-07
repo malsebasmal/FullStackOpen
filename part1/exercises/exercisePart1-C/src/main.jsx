ae67dc4b03f58f76480dfd45216af4876b54e4e2
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-07 16:27:36 -0500

    counter when refresh the page

--- part1/exercises/exercisePart1-C/src/main.jsx
+++ part1/exercises/exercisePart1-C/src/main.jsx
@@ -2,8 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let counter = 1
+
+const refresh = () => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App counter={counter}/>
+    </StrictMode>,
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
