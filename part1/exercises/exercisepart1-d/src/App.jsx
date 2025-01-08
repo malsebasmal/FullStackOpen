c924fc3bd13ceca5a5f8815e69b84db11dd6ceea
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-08 15:54:10 -0500

    remember react try used functional programming and this paradigma is based inmutable

--- part1/exercises/exercisepart1-d/src/App.jsx
+++ part1/exercises/exercisepart1-d/src/App.jsx
@@ -2,31 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
-
-  const handleLeftClick = () => {
-    setClicks({
-      ...clicks,
-      left: clicks.left + 1
-    })
-  }
-   
-  const handleRightClick = () => {
-    setClicks({
-      ...clicks,
-      left: clicks.right + 1
-    })
-  }
+  const [right, setRight] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [allClicks, setAll] = useState([])
   
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
