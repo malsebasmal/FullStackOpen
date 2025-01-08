20799d0c3a23a67d7e4360e6f65fc044340f1168
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-08 14:20:46 -0500

    complete the exercises and reading about basic states and events in react

--- part1/exercises/exercisePart1-C/src/App.jsx
+++ part1/exercises/exercisePart1-C/src/App.jsx
@@ -1,22 +1,33 @@
 import { useState } from "react"
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log("rendering with counter value", counter);
+  
 
-  const incrementByOne = () => setCounter(counter + 1)
-  const resetToCero = () => setCounter(0)
+  const incrementByOne = () => {
+    console.log("increment", counter);
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log("decrease", counter);
+    setCounter(counter - 1)
+  }
+  const resetToCero = () => {
+    console.log("reset", counter);
+    setCounter(0)
+  }
 
   return (
     <div>
-      <div>
-        {counter}
-      </div>
-      <button onClick={incrementByOne}>
-        plus
-      </button>
-      <button onClick={resetToCero}>
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button onClick={incrementByOne} text="plus"/>        
+      <Button onClick={decreaseByOne} text="minus"/>        
+      <Button onClick={resetToCero} text="reset" />
     </div>
   )
 }
