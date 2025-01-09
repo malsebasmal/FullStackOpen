c07ffe93d9ac00f9d336544b27830454e5815b34
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-09 11:53:01 -0500

    with handle control events

--- part1/exercises/exercise1.6-1.14/src/App.jsx
+++ part1/exercises/exercise1.6-1.14/src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleVoteGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleVoteNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleVoteBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      asd
+      <h1>Give Feedback</h1>
+      <button onClick={handleVoteGood}>Good</button>
+      <button onClick={handleVoteNeutral}>Neutral</button>
+      <button onClick={handleVoteBad}>Bad</button>
+
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
