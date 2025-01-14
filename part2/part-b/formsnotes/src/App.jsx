661cea414881ce2a605fae69d80b2993c3ef4da0
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-14 17:17:00 -0500

    conditional rendering and filter

--- part2/part-b/formsnotes/src/App.jsx
+++ part2/part-b/formsnotes/src/App.jsx
@@ -4,9 +4,8 @@ import './App.css'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    "new note"
-  )
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -24,11 +23,20 @@ const App = (props) => {
     setNewNote(event.target.value)    
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
