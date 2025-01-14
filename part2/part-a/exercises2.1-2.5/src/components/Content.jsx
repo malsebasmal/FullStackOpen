54bac5e450784cd04d3f77585d370abd07b04294
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-14 14:58:00 -0500

    complete the sum total with reduce

--- part2/part-a/exercises2.1-2.5/src/components/Content.jsx
+++ part2/part-a/exercises2.1-2.5/src/components/Content.jsx
@@ -1,14 +1,21 @@
 import Part from "./Part.jsx"
 
 const Content = ({parts}) => {
+  const total = parts.reduce((exercise, part) => {
+    return exercise + part.exercises
+  }, 0)
+
   return (
-    <ul>
-      {parts.map(part => 
-        <li key={part.id}>
-          <Part content={part}/>
-        </li>
-      )}
-    </ul>
+    <>
+      <ul>
+        {parts.map(part => 
+          <li key={part.id}>
+            <Part content={part}/>
+          </li>
+        )}
+      </ul>
+      <p>Total of {total} exercises</p>
+    </>
   )
 }
 
