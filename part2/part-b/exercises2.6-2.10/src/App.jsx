32255811e15eeb08e444215149ef98504d31b507
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-15 14:08:32 -0500

    add filtering in form with input

--- part2/part-b/exercises2.6-2.10/src/App.jsx
+++ part2/part-b/exercises2.6-2.10/src/App.jsx
@@ -3,14 +3,18 @@ import './App.css'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: "Arthur Hellas", number: "040-1234567"}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [searchPerson, setSearchPerson] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
-    const newNameObject = {name: newName, number: newNumber}
+    const newNameObject = {name: newName, number: newNumber , id: persons.length + 1 }
 
     if (persons.some(person => person.name === newNameObject.name)) {
       alert(`${newName} is already added to phonebook`)
@@ -29,9 +33,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setSearchPerson(event.target.value)
+  }
+  
+  const filteredPersons = searchPerson ?
+    persons.filter(person => person.name.toLowerCase().includes(searchPerson.toLocaleLowerCase())) :
+    persons
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with <input
+            onChange={handleFilterChange}
+            value={searchPerson}
+            type="text"
+            placeholder="type to search"
+          />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -53,8 +74,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => 
-          <li key={index}>
+        {filteredPersons.map(person => 
+          <li key={person.id}>
             {person.name} {person.number}
           </li>
         )}
