f4ce78442819cf7c1543d352651c1e49d29608b6
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-17 13:06:21 -0500

    getting data with axios - understanding how functions promises

--- part2/part-c/jsonserver/src/main.jsx
+++ part2/part-c/jsonserver/src/main.jsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+
+const promise = axios.get("http://localhost.localdomain:3001/notes")
+console.log(promise)
+promise.then(response => {
+  const notes = response.data
+  console.log(notes)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
