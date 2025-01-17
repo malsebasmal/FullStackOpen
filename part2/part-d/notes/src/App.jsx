0893963445b050698a9f10139c01dcb3c58017bb
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-17 16:20:45 -0500

    used axios for post ing

---
+++ part2/part-d/notes/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+import Note from './components/Note'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState([""])
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/notes")
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
+  
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1 
+    } 
+    
+    axios
+      .post("http://localhost:3001/notes", noteObject)
+      .then(response => {
+        console.log(response)
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+    <h1>Notes</h1>
+    <div>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all' }
+      </button>
+    </div>      
+    <ul>
+      {notesToShow.map(note => 
+        <Note key={note.id} note={note} />
+      )}
+    </ul>
+    <form onSubmit={addNote}>
+    <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form> 
+  </div>
+  )
+}
+
+export default App
