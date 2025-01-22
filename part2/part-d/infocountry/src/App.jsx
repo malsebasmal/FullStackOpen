c1375c9b84e473de933dbbd1469210e5035edcb8
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-22 18:12:54 -0500

    complete the exercise 2.19

--- part2/part-d/infocountry/src/App.jsx
+++ part2/part-d/infocountry/src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useState, useEffect } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const InfoCountryCondition = ({infoCountry}) => {
+  if (infoCountry.length >= 10) {
+    return <p>Too many matches, specify another filter</p>
+  } else if (infoCountry.length > 10) {
+    return (
+      <ul>
+        {infoCountry.map(country => (
+          <li key={country.name.common}>
+            {country.name.common}
+          </li>
+        ))}
+      </ul>
+    )
+  } else if (infoCountry.length === 1) {
+    const country = infoCountry[0]
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Area: {country.area}</p>
+        <p>languages:</p>
+        <ul>
+          {Object.entries(country.languages).map(([code, language]) => (
+            <li key={code}>{language}</li>
+          ))}
+        </ul>
+        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="150" />
+      </div>
+    )
+  }
+  return null
+}
+
+const CountryInfo = ({infoCountry}) => {
+  if (!infoCountry) {
+    return null
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <InfoCountryCondition infoCountry={infoCountry}/>
+  )
+}
+
+const App = () => {
+  const baseURL = "https://studies.cs.helsinki.fi/restcountries/api/all"
+
+  const [country, setCountry] = useState("")
+  const [infoCountry, setInfoCountry] = useState(null)
+
+  const handleChangeInput = (event) => setCountry(event.target.value)
+
+  useEffect(() => {
+    if (!country) {
+      setInfoCountry(null)
+      return
+    }
+
+    axios
+    .get(baseURL)
+    .then(response => {
+      const filteredCountries = response.data.filter(item => item.name.common.toLowerCase().startsWith(country.toLowerCase()))
+      setInfoCountry(filteredCountries)
+    })
+  }, [country])
+
+  return (
+    <div>
+      find countries
+      <input value={country} type="text" placeholder="find country" onChange={handleChangeInput} />
+      <CountryInfo infoCountry={infoCountry} />
+    </div>
   )
 }
 
