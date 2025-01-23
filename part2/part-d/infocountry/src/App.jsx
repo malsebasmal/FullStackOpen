386d7c73f606e5b26197e32ea7251b1027f923dd
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-23 14:19:43 -0500

    adding button show

--- part2/part-d/infocountry/src/App.jsx
+++ part2/part-d/infocountry/src/App.jsx
@@ -1,78 +1,98 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const InfoCountryCondition = ({infoCountry}) => {
-  if (infoCountry.length >= 10) {
-    return <p>Too many matches, specify another filter</p>
-  } else if (infoCountry.length > 10) {
-    return (
-      <ul>
-        {infoCountry.map(country => (
-          <li key={country.name.common}>
-            {country.name.common}
-          </li>
-        ))}
-      </ul>
-    )
-  } else if (infoCountry.length === 1) {
-    const country = infoCountry[0]
-    return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <p>Capital: {country.capital}</p>
-        <p>Area: {country.area}</p>
-        <p>languages:</p>
-        <ul>
-          {Object.entries(country.languages).map(([code, language]) => (
-            <li key={code}>{language}</li>
-          ))}
-        </ul>
-        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="150" />
-      </div>
-    )
-  }
-  return null
-}
-
-const CountryInfo = ({infoCountry}) => {
-  if (!infoCountry) {
-    return null
+const InfoOneCountry = ({ country }) => {
+  if (!country) {
+    return null;
   }
 
   return (
-    <InfoCountryCondition infoCountry={infoCountry}/>
-  )
-}
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <p>Languages:</p>
+      <ul>
+        {Object.entries(country.languages).map(([code, language]) => (
+          <li key={code}>{language}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        width="150"
+      />
+    </div>
+  );
+};
+
+const InfoCountryCondition = ({ infoCountry, onSelectCountry }) => {
+  if (!infoCountry) {
+    return null;
+  }
+
+  if (infoCountry.length === 1) {
+    const country = infoCountry[0];
+    return <InfoOneCountry country={country} />;
+  } else if (infoCountry.length < 10 && infoCountry.length > 1) {
+    return (
+      <ul>
+        {infoCountry.map((country) => (
+          <li key={country.name.common}>
+            {country.name.common}
+            <button onClick={() => onSelectCountry(country)}>show</button>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return <p>Too many matches, specify another filter</p>;
+  }
+};
 
 const App = () => {
-  const baseURL = "https://studies.cs.helsinki.fi/restcountries/api/all"
+  const baseURL = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
-  const [country, setCountry] = useState("")
-  const [infoCountry, setInfoCountry] = useState(null)
+  const [country, setCountry] = useState("");
+  const [infoCountry, setInfoCountry] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
-  const handleChangeInput = (event) => setCountry(event.target.value)
+  const handleChangeInput = (event) => setCountry(event.target.value);
 
   useEffect(() => {
     if (!country) {
-      setInfoCountry(null)
-      return
+      setInfoCountry(null);
+      return;
     }
 
-    axios
-    .get(baseURL)
-    .then(response => {
-      const filteredCountries = response.data.filter(item => item.name.common.toLowerCase().startsWith(country.toLowerCase()))
-      setInfoCountry(filteredCountries)
-    })
-  }, [country])
+    axios.get(baseURL).then((response) => {
+      const filteredCountries = response.data.filter((item) =>
+        item.name.common.toLowerCase().includes(country.toLowerCase())
+      );
+      setInfoCountry(filteredCountries);
+    });
+  }, [country]);
+
+  const handleSelectCountry = (country) => {
+    setSelectedCountry(country);
+  };
 
   return (
     <div>
       find countries
-      <input value={country} type="text" placeholder="find country" onChange={handleChangeInput} />
-      <CountryInfo infoCountry={infoCountry} />
+      <input
+        value={country}
+        type="text"
+        placeholder="find country"
+        onChange={handleChangeInput}
+      />
+      <InfoCountryCondition
+        infoCountry={infoCountry}
+        onSelectCountry={handleSelectCountry}
+      />
+      {selectedCountry && <InfoOneCountry country={selectedCountry} />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
