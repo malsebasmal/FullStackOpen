1020583e8007ae4e5c4343ac2379041d2f1cbfec
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-01-23 17:22:02 -0500

    part3 - a using express

---
+++ part3/part-a/serverinital/index.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get("/", (req, res) => {
+  res.send("<h1>Habla causa</h1>")
+})
+
+app.get("/notes", (req, res) => { 
+  res.json(notes)
+})
+
+app.get("/notes/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).send("not found webon")
+  }
+})
+
+app.delete("/notes/:id", (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
+
+app.post("/notes", (req, res) => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  const note = req.body
+  note.id = maxId + 1
+  notes = notes.concat(note)
+  res.json(note)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`running  http://localhost:${PORT}`)
+})
